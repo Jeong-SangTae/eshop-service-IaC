7c8a5052a4e78b12771baa7a531407136bbff064
Jeong-SangTae <wjdtkdxo87@naver.com>
2023-04-11 14:39:23 +0900

    update prefix on provider.tf terraform.tfvars

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "<<개인 버킷>>"        # my-s3-bucket
+    bucket = "wjdtkdxo87"        # my-s3-bucket
     key    = "eshop/terraform.tfstate"
-    region = "us-east-1"      # us-east-1
+    region = "us-westt-2"      # us-east-1
   }
   required_version = ">=1.1.3"
 }
